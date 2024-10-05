17bcb9d64af1020fca057fa646ae59082fc0d0c3
zarifmahir <zarifmahir21@gmail.com>
2024-10-05 12:35:29 +0600

    Update szz.cpp

--- szz.cpp
+++ szz.cpp
@@ -1,45 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
-#define int long long
-struct mice{
-  int s;
-  int x; 
-  int y; 
-  
-};
-double dist(int x1, int y1, int x2, int y2){
-  return sqrt(((x1-x2)*(x1-x2))+ ((y1-y2)*(y1-y2)));
+const int N=1e6;
+int x[16];
+int y[16];
+double s[16];
+int dis_arr[16];
+double dist(int x1,int y1,int x2, int y2)
+{
+    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 }
-void solve(){
-  int n; cin>>n;
-  vector<mice> m;
-  m.push_back({0,0,0});
-  for(int i=1; i<=n; i++){
-    int x, y, s;
-    cin>>x>>y>>s;
-    m.push_back({s, x, y});
-  }
-  sort(m.begin(), m.end());
-  double dis[n+1];
-  dis[1] = dist(0,0, m[1].x, m[1].y);
-  for(int i=2; i<=n; i++){
-      dis[i] = dist(m[i-1].x, m[i-1].y, m[i].x, m[i].y);
-  }
-  int cnt =0;
-  while(cnt!=n){
+
+int main(){
+int n,t;
+cin>>n;
+t=n;
+int i=0;
+while(t--)
+{   i++;
     
-  }
+    cin>>x[i]>>y[i]>>s[i];
+}
+dis_arr[1]=dist(x[1],y[1],0,0);
+for(int i=2;i<=n;i++)
+{
+    dis_arr[i]=dist(x[i],y[i],x[i-1],y[i-1]);
+}
+int m;
+cin>>m;
 
+double sum=dis_arr[1];
+for(int i=2;i<=n;i++)
+{
+    sum+=(dis_arr[i]/m);
+    m*=m;
 
 }
+double hi=s[1],lo=0,v_prev,v_cur;
+v_prev=0;
+v_cur=0;
+while(1)
+{
+    double mid=(hi+lo)/2;
+    v_prev=v_cur;
+    v_cur=dis_arr[0]/mid; 
+    if(fabs(v_cur-v_prev)<1e-3)
+    {
+        break;
+    }
+    if((sum/v_cur) >s[n])
+    {
+        lo=mid;
+    }
+    else{
+        hi=lo;
+    }
 
-signed main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  
-  int t; cin>>t;
-  while(t--) solve();
+    
+}
+
+cout<<v_cur;
 }
